6e42494179c49f7c1d8efbdbd91bbd60fb1050f6
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-11-13 10:43:32 +0000

    add custom User data throught registration controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+
+         #RELATIONS
+  #has_many :courses, dependent: :destroy
+  #has_many :materials
+  #has_many :levels
+  #has_many :exercices, through: :courses
+
+
+
+
+  #PRESENTE
+  validates :username,
+            :email, presence: true
+
+
+  #UNIQUENESS
+   validates :contact,
+             :email, uniqueness: true
+
+
+  #SLUG
+  extend FriendlyId
+    friendly_id :username, use: :slugged
+
+  def should_generate_new_friendly_id?
+    username_changed?
+  end
 end
