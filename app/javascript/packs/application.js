3bc3a7da1ed46eb4123b6b57c731cdb40b59528e
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-11-14 12:56:54 +0000

    add dashboard template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,30 @@ require("@rails/actiontext")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
+
 import "../css/application.css"
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
+
+
+document.addEventListener('turbolinks:load', () => {
+
+    document.addEventListener('click', () => {
+        let element = event.target.closest('.text-content')
+        if (!element) return;
+
+        element.classList.add('d-none')
+        element.nextElementSibling.classList.remove('d-none')
+    })
+
+    document.addEventListener('click', () => {
+        if (!event.target.matches('.cancel')) return;
+
+        event.preventDefault();
+
+        let element = event.target.closest('.form-content')
+
+        element.classList.add('d-none')
+        element.previousElementSibling.classList.remove('d-none')
+    })
+})
