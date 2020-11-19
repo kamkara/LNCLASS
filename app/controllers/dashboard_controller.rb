80bb54a06760d454a57b5d6b639b5c7310d9ff28
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-11-19 11:23:05 +0000

    add exercice collapse option to sidebar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,24 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_materials
+  before_action :find_levels
+
 
   def index
+    @materials = Material.all
+    @levels = Level.all
   end
+
+
+private
+
+    #enable material
+    def find_materials
+      @materials = Material.all
+    end
+
+    #enable level
+    def find_levels
+      @levels = Level.all
+    end
 end
