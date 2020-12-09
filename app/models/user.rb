e778a097f48e28482fdcd130a07827a8898f2e51
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-12-09 11:05:15 +0000

    add class name

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,9 @@ class User < ApplicationRecord
    validates :contact,
               :matricule,
              :email, uniqueness: true
+#CONSTANTE
+CLASSROOM= ["1", "2", "3", "4", "5" "6", "7", "8", "9", "10", "11", "12"]
+
 
 
 
