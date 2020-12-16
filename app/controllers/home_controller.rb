11f9f65287836cf480b27c9b0a42d4259662613e
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-12-16 21:06:07 +0000

    student:show only course for thier level and teacher can see all content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,18 @@
 class HomeController < ApplicationController
 before_action :find_levels
   def index
-    @courses = Course.all.order('created_at desc')
-    @materials = Material.all
+    if current_user.role=="student"
+      @materials = Material.all.order('created_at desc')
+      #select all course only user.level DESC
+      @courses = Course.where("course.level_id=?", current_user.level_id).order('created_at desc')
+      #where("course.level_id=?", current_user.level_id)
+    elsif current_user.role == 'teacher'
+      #select all course DESC
+      @courses = Courseall.all..order('created_at desc')
+      #where("course.material_id=?", current_user.material_id).order('created_at desc')
+      @materials = Material.all..order('created_at desc')
+      #where("course.material_id=?", current_user.material_id).order('created_at desc')
+    end
   end
 
 
