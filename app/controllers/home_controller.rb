e33117fabe2a4565968d35a9277bbbd518b7f18d
KAMKARA <lnclass.kamkara@gmail.com>
2020-12-23 16:45:49 +0000

    add code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,13 @@
 class HomeController < ApplicationController
 before_action :find_levels
-before_action :find_materials
   def index
     #content for student
     if current_user.role == "student"
       @courses = Course.where("level_id =?", current_user.level_id)
-      @materials = Material.all.order('created_at desc')
+      #@materials = Material.all.order('created_at desc')
     else #current_user is prof or admin...
       @courses = Course.all.order('created_at desc')
-      @materials = Material.all.order('created_at desc')
+      #@materials = Material.all.order('created_at desc')
       
     end
   end
@@ -20,7 +19,7 @@ private
       @levels = Level.all
    end
    def find_materials
-    #select all Material 
+    #select all
     @materials = Material.all
    end
 
