5efaddb4f5fdd9bf8f05df55b9fd4b086ac9c6b7
KAMKARA <lnclass.kamkara@gmail.com>
2020-12-23 15:56:04 +0000

    material fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,27 @@
 class HomeController < ApplicationController
 before_action :find_levels
+before_action :find_materials
   def index
-
-    @courses = Course.all.order('created_at desc')
-    @materials = Material.all.order('created_at desc')
+    #content for student
+    if current_user.role == "student"
+      @courses = Course.where("level_id =?", current_user.level_id)
+      @materials = Material.all.order('created_at desc')
+    else #current_user is prof or admin...
+      @courses = Course.all.order('created_at desc')
+      @materials = Material.all.order('created_at desc')
+      
+    end
   end
 
 
 private
+#find levels and material enable to Home controller
    def find_levels
       @levels = Level.all
+   end
+   def find_materials
+    #select all Material 
+    @materials = Material.all
    end
 
 end
