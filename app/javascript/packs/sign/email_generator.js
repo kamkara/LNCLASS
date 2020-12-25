8a9baa15480af05115e66b231fe92d9d96f6941e
KAMKARA <lnclass.kamkara@gmail.com>
2020-12-25 22:06:15 +0000

    add tailwind style to select fields on student form

---
+++ app/javascript/packs/sign/email_generator.js
@@ -0,0 +1,11 @@
+// au focus du champs contact, recupere le le matricule et construire 
+//l'email et l'attribuer a son champs pour les eleve .
+
+
+
+document.addEventListener('turbolinks;load', ()  => {
+// Variables
+let extension =" @gmail.com";
+getElementByid()
+})
+this.
