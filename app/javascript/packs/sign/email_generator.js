ec543354e4189c36cdcb9450c4d499a46b4ec0e3
KAMKARA <lnclass.kamkara@gmail.com>
2020-12-28 17:29:02 +0000

    Add config files.

--- app/javascript/packs/sign/email_generator.js
+++ app/javascript/packs/sign/email_generator.js
@@ -1,18 +1,10 @@
 // au focus du champs contact, recupere le le matricule et construire 
 //l'email et l'attribuer a son champs pour les eleve .
 
-
-
-document.addEventListener('turbolinks;load', ()  => {
-    
-// Variables
-let contact     = document.getElementById("student_contact");
-let extension   = "@gmail.com";
-let emailField  = document.getElementById("studente_email");
-let matricule   = document.getElementById("student_matricule").value.replace(/ /g, "");
-contact.addEventListener("onfocus",function(e){
-    emailField.value = matricule+extension;
-    console.log(emailField);
-});
-
+document.addEventListener("turbilinks;load", ()=>{
+    document.getElementById("studentContact").addEventListener("focus",function(){
+        //variables
+        let extension = "@gmail.com";
+        document.getElementById("studentEmail").value= document.getElementById("studentMatricule").value.replace(/ /g, "")+extension;
+    });
 })
